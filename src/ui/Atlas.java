7e9fa7657fff1a5b69e569bed725680dfd204464
Sofia Salcedo Romero <1105371907@u.icesi.edu.co>
2025-02-21 12:56:02 -0500

    entrega

--- src/ui/Atlas.java
+++ src/ui/Atlas.java
@@ -16,6 +16,8 @@ public class Atlas {
 
     // Declaracion de constantes (completar)
     final double COSTO_TRAYECTO_AVION = 250000;
+    final double COSTO_TRAYECTO_BUS = 80000;
+    final double COSTO_UNA_NOCHE = 150000;
 
 	private Atlas() {
 		escaner = new Scanner(System.in);
@@ -24,41 +26,60 @@ public class Atlas {
 	
 	public void run()
 	{   
-        // Declaracion de info a pedir del usuario (completar)
         String nombre;
-        // Saludo
         System.out.println("Bienvenido al programa de viajes EVENTOS ATLAS realizado por los estudiantes de Algoritmos y programacion I de la Universidad ICESI.");
-        // Pedir info al usuario (completar)
         System.out.println("Cual es tu nombre?");
         nombre = escaner.nextLine();
 
-        // ...
-        System.out.println("Bienvenido " + nombre + "!");
-        // ...
+        System.out.println("Cual es tu edad?");
+        edad = escaner.nextLine();
 
-        // Declaracion de salidas calculadas (completar)
-        double totalTransporte;
-        // Calculo de salidas mediante metodos (completar)
-        totalTransporte = calcularTotalTransporte(); // aqui podrian faltar parametros
-	}
+        System.out.println("Digita tu numero de identidad");
+        identidad = escaner.nextLine();
+
+        System.out.println("Digita la cuidad de residencia");
+        cuidad = escaner.nextLine();
+        
+        System.out.print("Ingrese cuantas noches desea hospedarse (min 1 noche - max 4 noches) ");
+        int noches = scanner.nextInt();
+
+        System.out.print("Ingrese su metodo de transporte preferido para este viaje (1: bus 2: avion)(para la vuelta): ");
+        int transporteIda = scanner.nextInt();
+
+        System.out.print("Ingrese su metodo de transporte preferido para este viaje (1: bus 2: avion)(para la ida): ");
+        int transporteVuelta = scanner.nextInt();
+
+        double costoHospedaje = calcularcostoHos(noches);
+        double costoTransporte = calcularCostoTransporte(transporteIda, transporteVuelta);
+        double costoTotal = calcularcosoTotal(costoHospedaje, costoTransporte);
+
+        System.out.println("El costo total del paquete es: $" + costoTotal);
+
+        System.out.println("Bienvenido" + nombre "tu edad: "+ edad "tu numero de indentidad es: " + identidad "y nos hablas desde la ciudad de: "+ ciudad );
+    }
+
+
 
 	public static void main(String[] args) {
 		Atlas mainApp = new Atlas();
 		mainApp.run();
 	}
-
-    // Completar metodos para calcular salidas
-
-    /**
-	 * Descripción: El método ... permite calcular ...
-	 * @param 
-	 * @return 
-	 */
-    public double calcularTotalTransporte() {
-        return 0; // Completar operacion
+    public static double calcularCostoHos(int noches) {
+        double costoTotal = COSTO_UNA_NOCHE * noches;
+        return costoTotal;
     }
 
-	
+   public static double calcularCostoTransporte(int transporteIda, int transporteVuelta) {
+        double TransporeteIda = (ida == 1) ? 80000 : 250000;
+        double TransporteVuelta = (vuelta == 1) ? 80000 : 250000; 
+        return transporteIda + TransporteVuelta;
+    }
+
+	    public static double calcularCostoTotal(double costoHospedaje, double costoTransporte) {
+        double costoBase = costoHospedaje + costoTransporte;
+        double costoServicio = costoBase * 0.2; 
+        return costoBase + costoServicio;
+    }
 
 	
 }
